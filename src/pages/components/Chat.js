5f262a213097c52003a223982ae49a80245501a6
dorinel.js <38764706+dorinelrushi@users.noreply.github.com>
2023-10-17 23:35:46 +0200

    Update Chat.js

--- src/pages/components/Chat.js
+++ src/pages/components/Chat.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import getRecipientEmail from '../../../utils/getRecipientEmail';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, db } from '../../../firebase';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-
-
+import React from "react";
+import getRecipientEmail from "../../../utils/getRecipientEmail";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../../firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 function Chat({ id, users }) {
+  const router = useRouter();
 
- const router = useRouter();
-
- const enterChat = () =>{
-    router.push(`chat/${id}`)
- }
+  const enterChat = () => {
+    router.push(`chat/${id}`);
+  };
 
   const [user] = useAuthState(auth);
+
   const [recipientSnapshot] = useCollection(
-    db.collection('users').where('email', '==', getRecipientEmail(users, user))
+    db.collection("users").where("email", "==", getRecipientEmail(users, user))
   );
 
   const recipient = recipientSnapshot?.docs?.[0]?.data();
@@ -26,12 +24,14 @@ function Chat({ id, users }) {
 
   return (
     <div>
-      <div className='flex gap-[5px] hover:bg-[#dedede] cursor-pointer' onClick={enterChat}>
+      <div
+        className="flex gap-[5px] hover:bg-[#dedede] cursor-pointer"
+        onClick={enterChat}
+      >
         {recipient ? (
           <Image src={recipient?.photoURL} width={100} height={100} />
         ) : (
           <div>
-
             <p>{recipientEmail[0]}</p>
           </div>
         )}
